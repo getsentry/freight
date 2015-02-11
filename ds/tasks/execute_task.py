074fe302be39c2acaea75acaa16b79acf8e68317
David Cramer <dcramer@gmail.com>
2015-02-10 19:30:59 -0800

    Throw python traceback in log on OSError

--- ds/tasks/execute_task.py
+++ ds/tasks/execute_task.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import logging
 import os
+import traceback
 
 from datetime import datetime
 from flask import current_app
@@ -55,6 +56,9 @@ def execute_task(task_id):
 
         try:
             provider.execute_task(workspace, task)
+        except OSError:
+            logbuffer.write(traceback.format_exc())
+            task.status = TaskStatus.failed
         except Exception:
             task.status = TaskStatus.failed
         else:
