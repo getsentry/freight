866026a5d2f89a8ac76a726720e4fbe812c94eb4
David Cramer <dcramer@gmail.com>
2015-02-13 13:25:04 -0800

    Fix arg passing to command

--- ds/providers/shell.py
+++ ds/providers/shell.py
@@ -12,11 +12,11 @@ class ShellProvider(Provider):
         }
 
     def execute(self, workspace, task):
-        command = task.provider_config['command'].format({
-            'environment': task.environment,
-            'sha': task.sha,
-            'ref': task.ref,
-            'task': task.name,
-        })
+        command = task.provider_config['command'].format(
+            environment=task.environment,
+            sha=task.sha,
+            ref=task.ref,
+            task=task.name,
+        )
 
         return workspace.run(command)
