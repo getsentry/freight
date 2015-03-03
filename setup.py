dae0fae91d04b94e3c35412ffd24dc98bb57d20d
David Cramer <dcramer@gmail.com>
2015-03-03 00:31:50 -0800

    Correct readme path

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     author_email='dcramer@gmail.com',
     url='https://github.com/getsentry/freight',
     description='A deployment service',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     install_requires=install_requires,
