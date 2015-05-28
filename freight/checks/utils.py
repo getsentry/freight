d5e27ffc73a692256dc3a80f3c36a0f9688d56ed
David Cramer <dcramer@gmail.com>
2015-05-28 12:00:24 +0200

    Add types to options

--- freight/checks/utils.py
+++ freight/checks/utils.py
@@ -23,6 +23,14 @@ def parse_checks_config(value):
                             instance.get_options().items())
         for option, option_values in all_options:
             value = config.get(option)
+            if value and option_values.get('type'):
+                try:
+                    config[option] = option_values['type'](value)
+                except (ValueError, TypeError):
+                    raise ApiError(
+                        message='Option "{}" is not a valid type for check: {}'.format(option, data['type']),
+                        name='invalid_check',
+                    )
             if option_values.get('required') and not value:
                 raise ApiError(
                     message='Missing required option "{}" for check: {}'.format(option, data['type']),
