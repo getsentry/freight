cbb4a34d8c5a73fda30997497c91d093dd4e2846
David Cramer <dcramer@gmail.com>
2015-11-25 17:17:36 -0800

    I give up

--- freight/config.py
+++ freight/config.py
@@ -85,7 +85,7 @@ def create_app(_read_config=True, **config):
 
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     app.config['SQLALCHEMY_POOL_SIZE'] = 5
-    app.config['SQLALCHEMY_MAX_OVERFLOW'] = 5
+    app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
     if 'SQLALCHEMY_DATABASE_URI' in os.environ:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
