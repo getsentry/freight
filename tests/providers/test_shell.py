34401d3237665cb5d330964b2efe2c543bd1dd85
David Cramer <dcramer@gmail.com>
2016-10-12 13:12:18 -0700

    Add prev_sha support to Shell provider

--- tests/providers/test_shell.py
+++ tests/providers/test_shell.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import pytest
 
 from freight import providers
+from freight.models import TaskStatus
 from freight.providers.utils import parse_provider_config
 from freight.exceptions import ApiError
 from freight.testutils import TestCase
@@ -22,13 +23,49 @@ class ShellProviderBase(TestCase):
 class ShellProviderTest(ShellProviderBase):
     def test_get_command(self):
         self.task.data['provider_config'] = {
-            'command': 'env={environment} task={params[task]} ref={ref} sha={sha} ssh_key={ssh_key}'
+            'command': (
+                'env={environment} '
+                'task={params[task]} '
+                'ref={ref} '
+                'sha={sha} '
+                'ssh_key={ssh_key} '
+                'prev_sha={prev_sha} '
+            )
         }
         result = self.provider.get_command(self.deploy, self.task, 'id_rsa').split(' ')
         assert 'env={}'.format(self.deploy.environment) in result
         assert 'ref={}'.format(self.task.ref) in result
         assert 'sha={}'.format(self.task.sha) in result
         assert 'task={}'.format(self.task.params['task']) in result
+        assert 'prev_sha=' in result
+        assert 'ssh_key=id_rsa' in result
+
+    def test_get_command_with_prev_sha(self):
+        prev_sha = 'b' * 40
+        prev_task = self.create_task(
+            app=self.app,
+            user=self.user,
+            sha=prev_sha,
+            status=TaskStatus.finished,
+        )
+        self.create_deploy(app=self.app, task=prev_task)
+
+        self.task.data['provider_config'] = {
+            'command': (
+                'env={environment} '
+                'task={params[task]} '
+                'ref={ref} '
+                'sha={sha} '
+                'ssh_key={ssh_key} '
+                'prev_sha={prev_sha} '
+            )
+        }
+        result = self.provider.get_command(self.deploy, self.task, 'id_rsa').split(' ')
+        assert 'env={}'.format(self.deploy.environment) in result
+        assert 'ref={}'.format(self.task.ref) in result
+        assert 'sha={}'.format(self.task.sha) in result
+        assert 'task={}'.format(self.task.params['task']) in result
+        assert 'prev_sha={}'.format(prev_sha) in result
         assert 'ssh_key=id_rsa' in result
 
 
