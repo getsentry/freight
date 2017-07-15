d46b2904d8a75de01fc82c0f1eb1c94a4d145c02
James Andrews <bdaddyja@gmail.com>
2017-07-14 17:34:08 -0700

    added more test for components.

--- static/tests/CreateDeploy.test.js
+++ static/tests/CreateDeploy.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import CreateDeploy from '../components/CreateDeploy.jsx';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
-jest.mock('../components/CreateDeploy.jsx')
 //TODO: figure out why snapshots are rendering null.
 test('CreateDeploy Snapshot', () => {
+  const appList = [{
+    "environments": {
+      "production": {
+        "defaultRef": "master"
+      },
+      "staging": {
+        "defaultRef": "HEAD"
+      },
+    },
+    "id": "1",
+    "name": "freight",
+    "repository": "https://github.com/getsentry/freight.git"
+  }];
   const component = renderer.create(
-    <CreateDeploy />
+    (<CreateDeploy appList={appList} />)
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
---
+++ static/tests/TaskSummary.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TaskSummary from '../components/TaskSummary.jsx';
+
+//TODO: figure out why snapshots are rendering null.
+test('TaskSummary Snapshot', () => {
+  const task = {
+    "app": {
+      id: "1",
+      "name": "freight"
+    },
+    "dataCreated": "2017-07-13T18:30:29.941695Z",
+    "dateFinished": "2017-07-13T18:30:45.484661Z",
+    "dateStarted": "2017-07-13T18:30:33.481500Z",
+    "duration": 12,
+    "environment": "production",
+    "estimatedDuration": 12,
+    "id": "797",
+    "name": "freight/production",
+    "number": 791,
+    "ref": "master",
+    "sha": "af56f6bc9843956c3ce92f9e7f9abb8c196340ee",
+    "status": "finished",
+    "user": {
+      "dateCreated": "2017-06-21T17:39:22.706427Z",
+      "id": "3",
+      "name": "james.andrews@sentry.io"
+    }
+  }
+  const context = {contextTypes: {
+      router: React.PropTypes.object.isRequired
+    }
+  }
+
+  const component = renderer.create(
+    <TaskSummary contextTypes={context.contextTypes} task={task} />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
