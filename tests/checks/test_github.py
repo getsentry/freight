f83bffd9e56c5d4687bcc98487f34820e417ea60
Matt Robenolt <matt@ydekproductions.com>
2017-07-20 14:34:58 -0700

    Fix issue where contexts aren't being ignored correctly
    When there's a partial success/failure, the contexts set was being
    mutated down to being empty. And empty meant the same thing as "all
    checks must pass". So this yields a copy of the contexts which are used
    strictly for checking against.

--- tests/checks/test_github.py
+++ tests/checks/test_github.py
@@ -123,3 +123,27 @@ class GitHubContextCheckTest(GitHubCheckBase):
 
         with pytest.raises(CheckFailed):
             self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_partial_failed_context(self):
+        body = json.dumps([
+            {
+                "state": "success",
+                "context": "travisci",
+                "description": "we did it",
+                "target_url": "example.com/build",
+            },
+            {
+                "state": "failing",
+                "context": "somethingelse",
+                "description": "we didn't do it",
+                "target_url": "example.com/build",
+            },
+        ])
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/statuses',
+                      body=body)
+
+        config = {'contexts': ['travisci'], 'repo': 'getsentry/freight'}
+
+        self.check.check(self.app, 'abcdefg', config)
