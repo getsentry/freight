bd747f140bc9fac2183cd380d5ae7d13b0dfcde9
James Andrews <bdaddyja@gmail.com>
2017-07-24 14:19:01 -0700

    Some formatting changes

--- static/mixins/polling.jsx
+++ static/mixins/polling.jsx
@@ -7,53 +7,53 @@ var PollingMixin = {
     };
   },
   pushNotification(){
+    let body;
     let {app, env, number} = this.props.params;
     const url = `/deploys/${app}/${env}/${number}`;
-    let body = ''
 
-    let options = {
+    if(this.state.task !== undefined){
+      body = `${/[^@]*/.exec(this.state.task.user.name)}'s deploy ${this.state.task.status}`;
+    }else{
+      body = `${/[^@]*/.exec(this.state.deploys[0].user.name)}'s deploy ${this.state.deploys[0].status}`;
+    }
+
+    const options = {
       body: body,
       icon: "../static/favicon.png"
     }
 
-    if(this.state.task != undefined){
-      body += `${/[^@]*/.exec(this.state.task.user.name)}'s deploy ${this.state.task.status}`
-    }else{
-      body += `${/[^@]*/.exec(this.state.deploys[0].user.name)}'s deploy ${this.state.deploys[0].status}`
-    }
-
     if (!("Notification" in window)) {
-    alert("This browser does not support system notifications");
+      alert("This browser does not support system notifications");
     }
 
   else if (Notification.permission === "granted") {
     if(this.state.task != undefined){
       let notification = new Notification(`${this.state.task.name}`, options)
         notification.onclick = function(event){
-        event.preventDefault();
-        window.open('http://freight.getsentry.net' + url)
+          event.preventDefault();
+          window.open('http://freight.getsentry.net' + url)
       }
     }
-    let notification = new Notification(`${this.state.deploys[0].name}`, options)
-      notification.onclick = function(event){
-      event.preventDefault();
-      window.open('http://freight.getsentry.net' + url)
+      let notification = new Notification(`${this.state.deploys[0].name}`, options)
+        notification.onclick = function(event){
+          event.preventDefault();
+          window.open('http://freight.getsentry.net' + url)
     }
   }
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === "granted" ) {
         if(this.state.task != undefined){
-          let notification = new Notification("Freight Deploy " + this.state.task.number , options)
+          let notification = new Notification(`${this.state.task.name}`, options)
             notification.onclick = function(event){
-            event.preventDefault();
-            window.open('http://freight.getsentry.net' + url)
+              event.preventDefault();
+              window.open('http://freight.getsentry.net' + url)
           }
         }
-        let notification = new Notification("Freight Deploy " + this.state.deploys[0].number, options)
-          notification.onclick = function(event){
-          event.preventDefault();
-          window.open('http://freight.getsentry.net' + url)
+          let notification = new Notification(`${this.state.deploys[0].name}`, options)
+            notification.onclick = function(event){
+              event.preventDefault();
+              window.open('http://freight.getsentry.net' + url)
           }
         }
       });
