764b3383718a0dbdfbfa2e2010858973add623c5
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-21 17:24:37 -0700

    WIP first commit - not printing out

---
+++ cloudbuilder-v1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Tool to track progress of a build in google cloud
+does this build exist?
+if yes then assign build id to GCB_ID
+
+using GCB_ID, print to stdout
+- progress of build
+- status of build
+- last 10 lines of log
+
+Jira ticket: https://getsentry.atlassian.net/browse/OPS-111
+"""
+
+import argparse
+import json
+import subprocess
+import sys
+
+REPO = "github-getsentry-brain"
+# SHA = $(git show-ref master --hash --heads) this command assumes that it is being run from repo
+SHA = "61ca573635fc6c7265baea7cba189a1a153ee955" # only hardcoded now for testing purposes
+
+data = subprocess.check_output(['gcloud', 'container', 'builds', 'list', '--filter', 'source.repo_source.repo_name='+REPO, 'AND source_provenance.resolved_repo_source.commit_sha='+SHA, 'AND status=SUCCESS"', '--format="value(id)"'])
+print data
+# def track_cloud_builder():
+#     pass
