423e5e214e253ab1c7ee0bd208b1fa7670fbb0fe
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-22 11:45:32 -0700

    WIP - added shlex, printing ID now

--- cloudbuilder-v1.py
+++ cloudbuilder-v1.py
@@ -12,6 +12,8 @@ using GCB_ID, print to stdout
 Jira ticket: https://getsentry.atlassian.net/browse/OPS-111
 """
 
+
+import shlex
 import argparse
 import json
 import subprocess
@@ -20,8 +22,9 @@ import sys
 REPO = "github-getsentry-brain"
 # SHA = $(git show-ref master --hash --heads) this command assumes that it is being run from repo
 SHA = "61ca573635fc6c7265baea7cba189a1a153ee955" # only hardcoded now for testing purposes
-
-data = subprocess.check_output(['gcloud', 'container', 'builds', 'list', '--filter', 'source.repo_source.repo_name='+REPO, 'AND source_provenance.resolved_repo_source.commit_sha='+SHA, 'AND status=SUCCESS"', '--format="value(id)"'])
+command = "gcloud container builds list --filter 'source.repo_source.repo_name=" + REPO + " AND source_provenance.resolved_repo_source.commit_sha=" + SHA + " AND status=SUCCESS' --format=value(id)"
+# command = 'gcloud container builds list --filter "source.repo_source.repo_name=github-getsentry-brain AND source_provenance.resolved_repo_source.commit_sha=61ca573635fc6c7265baea7cba189a1a153ee955 AND status=SUCCESS" --format="value(id)"'
+data = subprocess.check_output(shlex.split(command))
 print data
 # def track_cloud_builder():
 #     pass
