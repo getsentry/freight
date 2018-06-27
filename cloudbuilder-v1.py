e6b1a27a0c6d0ad4799fea7c535edfdc26a890f8
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-27 14:30:27 -0700

    Better logic. Add test for failure

--- cloudbuilder-v1.py
+++ cloudbuilder-v1.py
@@ -28,23 +28,28 @@ import sys
 REPO = "github-getsentry-brain"
 # SHA = $(git show-ref master --hash --heads) this command assumes that it is being run from same filesystem
 SHA = "82cce810fa298d99dabd176df9086a4ea043229d"  # only hardcoded now for testing purposes
-SHA_FAIL = "8474ed8fe807d4ec594606eeac8c551f24483f52" # SHA of build already failed
 COMMAND = """gcloud container builds list --filter 'source.repo_source.repo_name={} AND source_provenance.resolved_repo_source.commit_sha={}' --format='json' """.format(REPO, SHA)
 BUILD_DATA = json.loads(subprocess.check_output(shlex.split(COMMAND)))[0]
 BUILD_ID = BUILD_DATA['id']
 BUILD_STATUS = BUILD_DATA['status']
 
 # if build status is successful, do nothing; if not successful, print log.
-def check_build():
-    if BUILD_STATUS is not 'FAILURE':
+def check_build(STATUS, GCB_ID):
+    if STATUS is not 'FAILURE':
         print("""
 Build status is {} and ID is {}.
 See more details here:
     https://console.cloud.google.com/gcr/builds/{}
-        """.format(BUILD_STATUS, BUILD_ID, BUILD_ID))
+        """.format(STATUS, GCB_ID, GCB_ID))
     else:
+        log = subprocess.check_output(shlex.split("gcloud container builds log {}".format(GCB_ID)))
         print("""
-    Build failed. Printing log...""")
-        subprocess.check_output(shlex.split("gcloud container builds log {}".format(BUILD_ID)))
+Build failed. Printing log...
+{}
+        """.format(log))
 
-check_build()
+
+check_build(BUILD_STATUS, BUILD_ID)
+
+FAIL_ID = "e9c8057c-f873-4982-b71e-efa89b1e9ec3" # ID of build known to failed
+check_build('FAILURE', FAIL_ID)
