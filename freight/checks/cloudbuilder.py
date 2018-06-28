ed5bcf8ab65429306a1e29323bdb41de44e91478
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-28 14:56:32 -0700

    WIP - first pass

--- freight/checks/cloudbuilder.py
+++ freight/checks/cloudbuilder.py
@@ -1,16 +1,9 @@
-"""Tool to track progress of builds created
-by Google Cloud Platform Container Builder
-Jira ticket: https://getsentry.atlassian.net/browse/OPS-111
-"""
 from __future__ import absolute_import
 
-import argparse
 import json
 import shlex
 import subprocess
-import sys
 
-from freight import http
 from freight.exceptions import CheckFailed, CheckPending
 
 from .base import Check
@@ -18,39 +11,43 @@ from .base import Check
 __all__ = ['GCPContainerBuilderCheck']
 
 
-
-
 ERR_CHECK = '[{state}] {context}: {description} ({target_url})'
 ERR_MISSING_CONTEXT = '{} context was not found'
 
 
 class GCPContainerBuilderCheck(Check):
-    def check(self, app, sha, config):
-        COMMAND = """gcloud container builds list --filter 'source.repo_source.repo_name={} AND source_provenance.resolved_repo_source.commit_sha={}' --format='json' """.format(REPO, SHA)
-        BUILD_DATA = json.loads(subprocess.check_output(shlex.split(COMMAND)))[0]
+    """Track progress of builds created by Google Cloud Container Builder
+    Jira ticket: https: // getsentry.atlassian.net / browse / OPS - 111
+
+    Arguments:
+        Check {[type]} -- [description]
+
+    Raises:
+        CheckPending -- [description]
+        CheckFailed -- [description]
+    """
+
+    def check_build(self, app, sha, config, repo):
+        """Check build status
+        """
+        COMMAND = """gcloud container builds list
+            --filter 'source.repo_source.repo_name={} AND
+            source_provenance.resolved_repo_source.commit_sha={}'
+            --format='json' """.format(repo, sha)
+        BUILD_DATA = json.loads(
+            subprocess.check_output(shlex.split(COMMAND)))[0]
         BUILD_ID = BUILD_DATA['id']
         BUILD_STATUS = BUILD_DATA['status']
         # if build status is successful, do nothing; if not successful, print log.
-        def check_build(STATUS, GCB_ID):
-            if STATUS is not 'FAILURE':
-                print("""
-        Build status is {} and ID is {}.
-        See more details here:
+
+        if BUILD_STATUS is not 'FAILURE':
+            print("""Build status is {} and ID is {}.
+            See more details here:
             https://console.cloud.google.com/gcr/builds/{}
-                """.format(STATUS, GCB_ID, GCB_ID))
-            else:
-                log = subprocess.check_output(shlex.split("gcloud container builds log {}".format(GCB_ID)))
-                print("""
-        Build failed. Printing log...
-        {}
-                """.format(log))
-
-        check_build(BUILD_STATUS, BUILD_ID)
-
-        FAIL_ID = "e9c8057c-f873-4982-b71e-efa89b1e9ec3" # ID of build known to failed
-        check_build('FAILURE', FAIL_ID)
-        # if < some logic that means its pending >:
-        #     raise CheckPending('still goin')
-
-        # if < some logic that means its failing >:
-        #     raise CheckFailed( < some log lines > )
+            """.format(BUILD_STATUS, BUILD_ID, BUILD_ID))
+            raise CheckPending
+        else:
+            raise CheckFailed
+            log = subprocess.check_output(shlex.split("gcloud container builds log {}".format(BUILD_ID)))
+            print("""Build failed. Printing log...
+            {} """.format(log))
