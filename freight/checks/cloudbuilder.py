463710d6747d86145dad2c12ee399d6f34267b69
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-28 23:05:07 -0700

    WIP Fix function signature, change variables to local

--- freight/checks/cloudbuilder.py
+++ freight/checks/cloudbuilder.py
@@ -13,32 +13,39 @@ __all__ = ['GCPContainerBuilderCheck']
 
 class GCPContainerBuilderCheck(Check):
     """Track progress of builds created by Google Cloud Container Builder
-    Jira ticket: https: // getsentry.atlassian.net / browse / OPS - 111
+    Jira ticket: OPS-111
+
+    Add a Freight Check alongside the GitHub check that will:
+    1) Print the "waiting for build yadda yadda <link to build>"
+    2) Error if the build is not found, just like the GitHub check.
+    3) Exit cleanly if the build for the repo+sha is successful.
+    4) Print a sensible (50 lines to start) amount of the log if the build fails.
 
     Raises:
         CheckPending -- exception if build is in progress
         CheckFailed -- exception if build fails
     """
-    def check_build(self, app, sha, config, repo):
+    def check_build(self, app, sha, config):
         """Check build status
         """
-        COMMAND = """gcloud container builds list
+        repo = config['repo']
+        command = """gcloud container builds list
             --filter 'source.repo_source.repo_name={} AND
             source_provenance.resolved_repo_source.commit_sha={}'
             --format='json' """.format(repo, sha)
-        BUILD_DATA = json.loads(
-            subprocess.check_output(shlex.split(COMMAND)))[0]
-        BUILD_ID = BUILD_DATA['id']
-        BUILD_STATUS = BUILD_DATA['status']
+        build_data = json.loads(
+            subprocess.check_output(shlex.split(command)))[0]
+        build_id = build_data['id']
+        build_status = build_data['status']
 
-        if BUILD_STATUS is not 'FAILURE':
+        if build_status is not 'Failure':
             print("""Build status is {} and ID is {}.
             See more details here:
             https://console.cloud.google.com/gcr/builds/{}
-            """.format(BUILD_STATUS, BUILD_ID, BUILD_ID))
+            """.format(build_status, build_id, build_id))
             raise CheckPending
         else:
-            raise CheckFailed
-            log = subprocess.check_output(shlex.split("gcloud container builds log {}".format(BUILD_ID)))
+            log = subprocess.check_output(shlex.split("gcloud container builds log {}".format(build_id)))
             print("""Build failed. Printing log...
             {} """.format(log))
+            raise CheckFailed
