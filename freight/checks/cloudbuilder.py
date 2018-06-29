306000bfbe2b5a588ebb33a42606ffb3eae5f723
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-06-29 15:14:15 -0700

    WIP

--- freight/checks/cloudbuilder.py
+++ freight/checks/cloudbuilder.py
@@ -4,6 +4,7 @@ import json
 import shlex
 import subprocess
 
+from freight import http
 from freight.exceptions import CheckFailed, CheckPending
 
 from .base import Check
@@ -28,15 +29,25 @@ class GCPContainerBuilderCheck(Check):
     def check_build(self, app, sha, config):
         """Check build status
         """
+        api_root = 'https://cloudbuild.googleapis.com/v1/projects/internal-sentry/builds'
+        commit_sha = "f8626d73efdd496c101f386179616c6229194a8e" # this comes from the check() method
+
+        oauth_command = "gcloud auth application-default print-access-token"
+        oauth_token = subprocess.check_output(shlex.split(oauth_command)).rstrip()
+
+        params = {'filter': 'sourceProvenance.resolvedRepoSource.commitSha = "{}"'.format(commit_sha)}
+        headers = {'Accepts': 'application/json', 'Authorization': 'Bearer {}'.format(oauth_token)}
+
+        build_data = http.get(api_root, headers=headers, params=params).json()
+
         repo = config['repo']
         command = """gcloud container builds list
             --filter 'source.repo_source.repo_name={} AND
             source_provenance.resolved_repo_source.commit_sha={}'
             --format='json' """.format(repo, sha)
-        build_data = json.loads(
-            subprocess.check_output(shlex.split(command)))[0]
-        build_id = build_data['id']
-        build_status = build_data['status']
+
+        build_id = build_data['builds'][0]['id']
+        build_status = build_data['builds'][0]['status']
 
         if build_status is not 'Failure':
             print("""Build status is {} and ID is {}.
