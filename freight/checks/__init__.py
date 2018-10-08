cae183f23d3b67e1021efb04976ec7dc0e478b15
Anton Ovchinnikov <anton@tonyo.info>
2018-10-08 22:09:55 +0200

    feat: Add github-apps check type (#152)

--- freight/checks/__init__.py
+++ freight/checks/__init__.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 from .base import Check  # NOQA
 from .manager import CheckManager
 from .github import GitHubContextCheck
+from .github_apps import GitHubAppsContextCheck
 from .cloudbuilder import GCPContainerBuilderCheck
 
 manager = CheckManager()
 manager.add('github', GitHubContextCheck)
+manager.add('github-apps', GitHubAppsContextCheck)
 manager.add('cloudbuilder', GCPContainerBuilderCheck)
 
 get = manager.get
