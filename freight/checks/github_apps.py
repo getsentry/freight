bd4e94167971d6bb47da8bd7663613ee1d43412a
Anton Ovchinnikov <anton@tonyo.info>
2018-10-08 21:26:20 +0200

    feat: Add github-apps check type

---
+++ freight/checks/github_apps.py
@@ -0,0 +1,80 @@
+from __future__ import absolute_import
+
+__all__ = ['GitHubAppsContextCheck']
+
+
+from flask import current_app
+
+from freight import http
+from freight.exceptions import CheckFailed, CheckPending
+
+from .base import Check
+
+ERR_CHECK = '[{status}] {name}: {description} ({details_url})'
+ERR_MISSING_CONTEXT = '{} context was not found'
+
+
+class GitHubAppsContextCheck(Check):
+    def get_options(self):
+        return {
+            'api_root': {'required': False},
+            # if contexts is not set it will require all of them be valid
+            'contexts': {'required': False},
+            'repo': {'required': True},
+        }
+
+    def check(self, app, sha, config):
+        token = current_app.config['GITHUB_TOKEN']
+        if not token:
+            raise CheckFailed('GITHUB_TOKEN is not set')
+
+        api_root = (
+            config.get('api_root') or current_app.config['GITHUB_API_ROOT']
+        ).rstrip('/')
+
+        all_contexts = set(config.get('contexts') or [])
+        contexts = all_contexts.copy()
+        repo = config['repo']
+
+        url = '{api_root}/repos/{repo}/commits/{ref}/check-runs'.format(
+            api_root=api_root,
+            repo=repo,
+            ref=sha,
+        )
+
+        headers = {
+            'Accept': 'application/vnd.github.antiope-preview+json',
+            'Authorization': 'token {}'.format(token),
+        }
+
+        resp = http.get(url, headers=headers)
+
+        check_runs_dict = resp.json()
+        if not check_runs_dict or not check_runs_dict.get('total_count'):
+            raise CheckFailed('No contexts were present in GitHub')
+
+        valid_contexts = set()
+        for check_run in check_runs_dict.get('check_runs', []):
+            check_name = check_run['name']
+            if all_contexts and check_name not in all_contexts:
+                continue
+            if check_run['conclusion'] == 'success':
+                valid_contexts.add(check_name)
+                try:
+                    contexts.remove(check_name)
+                except KeyError:
+                    pass
+            if check_name in valid_contexts:
+                continue
+            if contexts and check_name not in contexts:
+                continue
+
+            check_run['description'] = check_run['output']['title']
+            if check_run['status'] in ['queued', 'in_progress']:
+                raise CheckPending(ERR_CHECK.format(**check_run))
+            elif check_run['conclusion'] != 'success':
+                raise CheckFailed(ERR_CHECK.format(**check_run))
+            contexts.remove(check_name)
+
+        if contexts:
+            raise CheckFailed(ERR_MISSING_CONTEXT.format(iter(contexts).next()))
---
+++ tests/checks/test_github_apps.py
@@ -0,0 +1,178 @@
+from __future__ import absolute_import
+
+import json
+import responses
+import pytest
+
+from freight import checks
+from freight.exceptions import CheckFailed, CheckPending
+from freight.testutils import TestCase
+
+
+class GitHubAppsCheckBase(TestCase):
+    def setUp(self):
+        self.check = checks.get('github-apps')
+        self.user = self.create_user()
+        self.repo = self.create_repo()
+        self.app = self.create_app(repository=self.repo)
+
+
+class GitHubAppsContextCheckTest(GitHubAppsCheckBase):
+    @responses.activate
+    def test_not_passing(self):
+        body = json.dumps({
+            "total_count": 1,
+            "check_runs": [
+                {
+                    "status": "completed",
+                    "conclusion": "failure",
+                    "name": "Travis CI - Branch",
+                    "details_url": "https://travis/87026985",
+                    "output": {
+                        "title": "Build Failed",
+                    }
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'contexts': ['Travis CI - Branch'], 'repo': 'getsentry/freight'}
+
+        with pytest.raises(CheckFailed):
+            self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_not_finished(self):
+        body = json.dumps({
+            "total_count": 1,
+            "check_runs": [
+                {
+                    "status": "in_progress",
+                    "name": "Travis CI - Branch",
+                    "conclusion": None,
+                    "details_url": "https://travis/87026985",
+                    "output": {
+                        "title": "Build Failed",
+                    }
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'contexts': ['Travis CI - Branch'], 'repo': 'getsentry/freight'}
+
+        with pytest.raises(CheckPending):
+            self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_missing_required_context(self):
+        body = json.dumps({
+            "total_count": 1,
+            "check_runs": [
+                {
+                    "status": "in_progress",
+                    "name": "Travis CI - Branch",
+                    "conclusion": None,
+                    "details_url": "https://travis/87026985",
+                    "output": {
+                        "title": "Build Failed",
+                    }
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'contexts': ['other'], 'repo': 'getsentry/freight'}
+
+        with pytest.raises(CheckFailed):
+            self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_success(self):
+        body = json.dumps({
+            "total_count": 1,
+            "check_runs": [
+                {
+                    "status": "completed",
+                    "conclusion": "success",
+                    "name": "Travis CI - Branch"
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'contexts': ["Travis CI - Branch"], 'repo': 'getsentry/freight'}
+
+        self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_no_contexts_with_none_defined(self):
+        body = json.dumps({"total_count": 0, "check_runs": []})
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'repo': 'getsentry/freight'}
+
+        with pytest.raises(CheckFailed):
+            self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_any_failing_context_with_none_define(self):
+        body = json.dumps({
+            "total_count": 1,
+            "check_runs": [
+                {
+                    "status": "completed",
+                    "conclusion": "failure",
+                    "name": "Travis CI - Branch",
+                    "details_url": "https://travis/87026985",
+                    "output": {
+                        "title": "Build Failed",
+                    }
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'repo': 'getsentry/freight'}
+
+        with pytest.raises(CheckFailed):
+            self.check.check(self.app, 'abcdefg', config)
+
+    @responses.activate
+    def test_partial_failed_context(self):
+        body = json.dumps({
+            "total_count": 2,
+            "check_runs": [
+                {
+                    "status": "completed",
+                    "name": "Travis CI - Branch",
+                    "conclusion": "success",
+                    "details_url": "https://travis/87026985",
+                },
+                {
+                    "status": "completed",
+                    "name": "somethingelse",
+                    "conclusion": "failure",
+                    "details_url": "https://travis/87026985",
+                }
+            ]
+        })
+
+        responses.add(responses.GET, 'https://api.github.com/repos/getsentry/freight/commits/abcdefg/check-runs',
+                      body=body)
+
+        config = {'contexts': ["Travis CI - Branch"], 'repo': 'getsentry/freight'}
+
+        self.check.check(self.app, 'abcdefg', config)
