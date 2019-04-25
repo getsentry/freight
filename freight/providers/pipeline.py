f3025854fcce73d0f47129895cb349d12dfbd1d7
Matt Robenolt <matt@ydekproductions.com>
2019-04-25 13:30:25 -0700

    cleanups and comments

--- freight/providers/pipeline.py
+++ freight/providers/pipeline.py
@@ -41,6 +41,10 @@ class StepFailed(Exception):
     pass
 
 
+class AuthenticationError(Exception):
+    pass
+
+
 class PipelineProvider(Provider):
     def get_options(self):
         return {
@@ -91,6 +95,7 @@ class PipelineProvider(Provider):
 
 
 def load_kube_credentials(config: dict) -> client.ApiClient:
+    # If a context is specified, attempt to use this first.
     try:
         return new_client_from_config(context=config["context"])
     except KeyError:
@@ -99,14 +104,18 @@ def load_kube_credentials(config: dict) -> client.ApiClient:
     try:
         credentials = config["credentials"]
     except KeyError:
-        return
+        raise AuthenticationError("Missing kubernetes context.")
 
-    assert credentials["kind"] == "gcloud"
+    if credentials["kind"] not in ("gcloud",):
+        raise AuthenticationError(f"Unknown kubernetes kind: {credentials['kind']}")
 
     return load_kube_credentials_gcloud(credentials)
 
 
 def load_kube_credentials_gcloud(credentials: dict) -> client.ApiClient:
+    # Try to pull credentials from gcloud, but first checking if there
+    # is a context, using their auto generated naming scheme, to avoid
+    # calling `gcloud` every time, if we've already authed before.
     from subprocess import check_call, DEVNULL
 
     cluster = credentials["cluster"]
@@ -139,10 +148,10 @@ def load_kube_credentials_gcloud(credentials: dict) -> client.ApiClient:
     return new_client_from_config(context=context)
 
 
-def run_step(step: dict, context: PipelineContext) -> bool:
-    assert step["kind"] in ("Shell", "KubernetesDeployment", "KubernetesJob"), step[
-        "kind"
-    ]
+def run_step(step: dict, context: PipelineContext):
+    if step["kind"] not in ("Shell", "KubernetesDeployment", "KubernetesJob"):
+        raise StepFailed(f"Unknown step kind: {step['kind']}")
+
     watchers = {
         "Shell": run_step_shell,
         "KubernetesDeployment": run_step_deployment,
@@ -150,7 +159,12 @@ def run_step(step: dict, context: PipelineContext) -> bool:
     }[step["kind"]](step, context)
     if not watchers:
         return
-    changes = bool(watchers)
+
+    # Steps may return a list of "watchers". A watcher is meant to
+    # watch over an async operation and wait until they are finished.
+
+    # Iterate over all watchers, waiting for state to be True, and
+    # printing out their status until they finish.
     while watchers:
         for watcher, state in watchers:
             status, success = watcher()
@@ -163,12 +177,12 @@ def run_step(step: dict, context: PipelineContext) -> bool:
             if success:
                 watchers.remove((watcher, state))
                 if not watchers:
-                    break
+                    return
         sleep(0.5)
-    return changes
 
 
 def run_step_deployment(step: dict, context: PipelineContext) -> List[Callable]:
+    # Execute a Kubernetes Deployment
     context.workspace.log.info(f"Running Deployment: {repr(step)}")
 
     api = client.AppsV1beta1Api(context.kube.client)
@@ -179,6 +193,22 @@ def run_step_deployment(step: dict, context: PipelineContext) -> List[Callable]:
 
     watchers = []
 
+    # First, we collect a list of Deployments from kubernetes based on
+    # the `selector`. This may return 0, 1, or many Deployments.
+
+    # For each Deployment, we scan for matching containers by name. So
+    # it's possible that a Deployment matches a selector, but does not
+    # have a container that matches. This is expected behavior, and moves
+    # along gracefully. This allows a wider selector, and refine it by only
+    # updating containers that match.
+
+    # When a container is found within a Deployment, we patch the `image`.
+    # This is roughly equivalent to doing `kubectl set image ...`. We also
+    # patch in metadata about the deploy itself in the annotations on both
+    # the Deployment and the PodTemplate. This decision was made so that
+    # a bump of the annotations would cause a rolling restart of the service,
+    # even if the image doesn't change. This means a re-deploy of a service
+    # is not entirely a no-op, but will restart all Deployments still.
     for deployment in api.list_namespaced_deployment(**selector).items:
         namespace = deployment.metadata.name
         name = deployment.metadata.name
@@ -188,6 +218,10 @@ def run_step_deployment(step: dict, context: PipelineContext) -> List[Callable]:
                 if container.name == c["name"]:
                     container.image = c["image"]
                     changes = True
+
+        # If there are any changes at all in this deployment, we need
+        # to patch in our freight annotations, and trigger the PATCH call,
+        # as well as set up the watcher to keep track of its rollout.
         if changes:
             if deployment.metadata.annotations is None:
                 deployment.metadata.annotations = {}
@@ -221,11 +255,14 @@ def run_step_deployment(step: dict, context: PipelineContext) -> List[Callable]:
 
 
 def run_step_shell(step: dict, context: PipelineContext):
+    # Execute a shell command within our workspace
     command = format_task(step["command"], context.task)
     context.workspace.run(command, env=step.get("env"))
 
 
 def format_task(data, task: TaskContext):
+    # Recursively formatting strings that may
+    # contain taskcontext vars.
     if isinstance(data, str):
         return data.format(**asdict(task))
     if isinstance(data, list):
@@ -236,6 +273,13 @@ def format_task(data, task: TaskContext):
 
 
 def make_job_spec(step: dict, task: TaskContext) -> dict:
+    # Translate our customer KubernetesJob, which is just a subset of
+    # allowed Kubernetes Job configs, into a real Job spec. This subset is
+    # done so that we can control better what a Job means, including an
+    # auto generated name, and enforcing the use of a single container.
+    # The properties allowed to be defined are:
+    #   name*, namespace, image*, args, env, volumeMounts, resources, volumes
+    # * = required
     return {
         "kind": "Job",
         "metadata": {
@@ -265,6 +309,21 @@ def make_job_spec(step: dict, task: TaskContext) -> dict:
 
 
 def run_step_job(step: dict, context: PipelineContext):
+    # Run a one-off Job in Kubernetes and track it through to completion.
+    # Running a job in Kubernetes is a bit non-trivial, and involves the
+    # following:
+    #  * Create the Job spec
+    #  * Querying for the Pod created by the Job until it is in a state
+    #    other than Pending. This involves using the label: `job-name={jobname}`
+    #  * Once we're past Pending, the Pod is either in Running, Succeeded,
+    #    or Failed state. At this point, we read it's container logs until
+    #    the pod exits.
+    #  * Once the Pod exits, and we have fully read the logs, we are safe
+    #    to proceed with reporting the step as success or failure.
+    #  * When all said and done, we delete the Job, with it's children Pods
+    #    to avoid these collecting and hanging around. It's still possible
+    #    a Job may get orphaned, but these will eventually get cleaned up by
+    #    Kubernetes, so it's not a huge deal if this happens.
     context.workspace.log.info(f"Running Job: {repr(step)}")
 
     job_spec = make_job_spec(step, context.task)
@@ -277,6 +336,7 @@ def run_step_job(step: dict, context: PipelineContext):
     )
     pod_name = None
     try:
+        # Find our Pod that is spawned from this Job
         pods = (
             client.CoreV1Api(context.kube.client)
             .list_namespaced_pod(namespace=namespace, label_selector=f"job-name={name}")
@@ -285,17 +345,27 @@ def run_step_job(step: dict, context: PipelineContext):
         assert len(pods) == 1, len(pods)
         pod_name = pods[0].metadata.name
         context.workspace.log.info(f"Waiting for Pod {pod_name}")
+
+        # Now we begin our loop and shitty state machine
         read_logs = False
         while True:
             pod = client.CoreV1Api(context.kube.client).read_namespaced_pod(
                 namespace=namespace, name=pod_name
             )
+            # If we have successfully read our logs, we're ready to report
+            # status.
             if read_logs:
                 if pod.status.phase == "Failed":
                     raise StepFailed("Failed")
-                break
+                return
 
             if pod.status.phase == "Pending":
+                # If we are in a Pending state, we need to also check
+                # if we're in the ContainerCreating phase of the container inside.
+                # For any other event, like, an ErrImagePull, the Pod will
+                # forever be left in Pending, but the container inside has errored.
+                # So the only valid state to continue waiting is ContainerCreating.
+                # Any other state is considered a failure.
                 if (
                     pod.status.container_statuses[0].state.waiting.reason
                     == "ContainerCreating"
@@ -308,6 +378,17 @@ def run_step_job(step: dict, context: PipelineContext):
                     )
 
             if pod.status.phase in ("Running", "Succeeded", "Failed"):
+                # At this point, our Pod has either begun running or finished.
+                # For any case, we want to begin to tail (follow) logs until the
+                # Pod exits. This works for both a running Pod, and a finished Pod
+                # the same.
+
+                # HACK: When _preload_context is passed, this causes the kubernetes
+                # API here to return a urllib3 HTTPResponse object directly ready
+                # to be streamed, so the following use of `resp.stream()` and
+                # `resp.release_conn()` are just standard urllib3 behaviors.
+                # This is entirely undocumented behavior afaik, and the only way to
+                # actually get `follow=True` to work.
                 resp = client.CoreV1Api(context.kube.client).read_namespaced_pod_log(
                     namespace=namespace,
                     name=pod_name,
@@ -323,6 +404,10 @@ def run_step_job(step: dict, context: PipelineContext):
                 sys.stdout.write("\n")
                 sys.stdout.flush()
                 read_logs = True
+                # When finished reading logs, we need to loop back around
+                # so we can query the final state of the Pod, in the case that
+                # it was Running when we got here, we don't know if it succeeded
+                # or failed.
     finally:
         context.workspace.log.info(f"Deleting Job {namespace}/{name}")
         client.BatchV1Api(context.kube.client).delete_namespaced_job(
@@ -333,6 +418,8 @@ def run_step_job(step: dict, context: PipelineContext):
 def rollout_status_deployment(
     api: client.AppsV1beta1Api, name: str, namespace: str, generation: int
 ) -> Tuple[str, bool]:
+    # tbh this is mostly ported from Go into Python from:
+    # https://github.com/kubernetes/kubernetes/blob/master/pkg/kubectl/rollout_status.go#L76-L92
     deployment = api.read_namespaced_deployment(name=name, namespace=namespace)
     if generation <= deployment.status.observed_generation:
         replicas = deployment.spec.replicas
