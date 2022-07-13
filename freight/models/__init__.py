c0b3c6fb3330de1e267448ec2ef483d245ea5bc3
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-13 11:04:51 -0700

    ref: Avoid usage of import_submodules to help with LSP (#308)

--- freight/models/__init__.py
+++ freight/models/__init__.py
@@ -1,4 +1,8 @@
-from freight.utils.imports import import_submodules
-
-
-import_submodules(locals(), __name__, __path__)
+from .app import *  # NOQA
+from .deploy import *  # NOQA
+from .deploysequence import *  # NOQA
+from .logchunk import *  # NOQA
+from .repository import *  # NOQA
+from .task import *  # NOQA
+from .taskconfig import *  # NOQA
+from .user import *  # NOQA
