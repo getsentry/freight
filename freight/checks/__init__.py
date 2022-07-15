f82a06fa7c574e7ecda20a28f06e334849bfdfa2
Joshua Li <joshuarli98@gmail.com>
2022-07-15 14:01:54 -0700

    remove unused gihtub check, we only use github-apps

--- freight/checks/__init__.py
+++ freight/checks/__init__.py
@@ -1,11 +1,9 @@
 from .base import Check  # NOQA
 from .manager import CheckManager
-from .github import GitHubContextCheck
 from .github_apps import GitHubAppsContextCheck
 from .cloudbuilder import GCPContainerBuilderCheck
 
 manager = CheckManager()
-manager.add("github", GitHubContextCheck)
 manager.add("github-apps", GitHubAppsContextCheck)
 manager.add("cloudbuilder", GCPContainerBuilderCheck)
 
