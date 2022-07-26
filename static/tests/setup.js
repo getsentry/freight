d05ac34f10beb53fc6b9286b871b89d09c200e54
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-26 15:14:19 -0700

    ref(js): Remove jquery (#322)

--- static/tests/setup.js
+++ static/tests/setup.js
@@ -1,11 +1,8 @@
 // eslint-disable-next-line no-restricted-imports
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import jQuery from 'jquery';
 
 /**
  * Enzyme configuration
  */
 Enzyme.configure({adapter: new Adapter()});
-
-window.$ = window.jQuery = jQuery;
