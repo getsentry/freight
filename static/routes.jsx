5ea6bcd49b6f663f7dbcae433135560a94a14173
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-26 15:20:24 -0700

    ref(js): Delete app settings page (#324)

--- static/routes.jsx
+++ static/routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 
 import AppDetails from './components/AppDetails';
-import AppSettings from './components/AppSettings';
 import CreateDeploy from './components/CreateDeploy';
 import Layout from './components/Layout';
 import Overview from './components/Overview';
@@ -16,7 +15,6 @@ const routes = () => {
       <Route path="/deploy" component={CreateDeploy} />
       <Route path="/tasks/:app/:env/:number" component={TaskDetails} />
       <Route path="/deploys/:app/:env/:number" component={TaskDetails} />
-      <Route path="/:app/settings" component={AppSettings} />
       <Route path="/:app/:env/:number" component={TaskDetails} />
       <Route path="/:app" component={AppDetails} />
       <Route path="*" component={RouteNotFound} />
