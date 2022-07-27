42060880e4fe7b17a6ba091eef356aadf7307621
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-27 16:56:12 -0700

    ref(js): Convert Overview to a FC
    Also extracts the deploy notification logic into a hook

---
+++ static/hooks/useDeployFinishedNotification.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useRef} from 'react';
+
+import pushNotification from 'app/utils/pushNotification';
+
+/**
+ * Produces a mapping of deploy.id -> deploy.status
+ */
+function mapDeployStatus(deploys) {
+  return Object.fromEntries(deploys.map(deploy => [deploy.id, deploy.status]));
+}
+
+/**
+ * Triggers a notification when any of the deploys in the list become completed
+ */
+function useDeployFinishedNotification(deploys) {
+  const lastDeployStatus = useRef(null);
+
+  if (lastDeployStatus.current === null) {
+    lastDeployStatus.current = mapDeployStatus(deploys);
+  }
+
+  useEffect(() => {
+    deploys.forEach(deploy => {
+      // Did the deploy status change from in_progress to finished?
+      if (
+        lastDeployStatus.current[deploy.id] === 'in_progress' &&
+        deploy.status === 'finished'
+      ) {
+        const {name} = deploy.app;
+        const {environment, number} = deploy;
+        const path = `/deploys/${name}/${environment}/${number}`;
+        pushNotification(deploy, path);
+      }
+    });
+
+    lastDeployStatus.current = mapDeployStatus(deploys);
+  }, [deploys]);
+
+  // Request permission for notifications
+  useEffect(() => {
+    if (window.Notification && Notification.permission !== 'denied') {
+      Notification.requestPermission();
+    }
+  }, []);
+}
+
+export default useDeployFinishedNotification;
