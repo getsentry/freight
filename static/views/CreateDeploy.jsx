b60938dcf1f3395d13271ab744441575efd63046
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-27 17:43:39 -0700

    ref(js): Convert CreateDeploy to a FC

--- static/views/CreateDeploy.jsx
+++ static/views/CreateDeploy.jsx
@@ -1,172 +1,135 @@
 import * as React from 'react';
 import {browserHistory} from 'react-router';
-import PropTypes from 'prop-types';
 
 import api from 'app/api';
 
-class CreateDeploy extends React.Component {
-  static propTypes = {
-    appList: PropTypes.array,
-  };
+function gotoDeploy(deploy) {
+  const {app, environment, number} = deploy;
+  browserHistory.push(`/deploys/${app.name}/${environment}/${number}`);
+}
 
-  constructor(props) {
-    super(props);
-    const appList = props.appList;
-    const defaultApp = appList.length !== 0 ? appList[0] : null;
-    const app = props.location.query?.app
-      ? props.location.query.app
-      : defaultApp
-      ? defaultApp.name
-      : null;
-    const envMap = app
-      ? appList.filter(appObj => appObj.name === app)[0].environments || {}
-      : {};
-    const envList = Object.keys(envMap);
-    const env = envList.length ? envList[0] : null;
-    const defaultRef = env ? envMap[env].defaultRef : 'master';
+function CreateDeploy({location, appList = []}) {
+  const firstApp = appList.length !== 0 ? appList[0] : null;
 
-    this.state = {
-      app,
-      env,
-      envMap,
-      ref: defaultRef,
-      submitInProgress: false,
-      submitError: null,
-    };
-  }
+  const defaultApp = location.query?.app
+    ? location.query.app
+    : firstApp
+    ? firstApp.name
+    : null;
 
-  onChangeApplication = e => {
-    const val = e.target.value;
-    const envMap = val
-      ? this.props.appList.filter(app => {
-          return app.name === val;
-        })[0].environments || {}
-      : {};
-    const envList = Object.keys(envMap);
-    const env = envList.length ? envList[0] : null;
-    this.setState({
-      app: val,
-      envMap,
-      env,
-      ref: env ? env.defaultRef : 'master',
+  const [app, setApp] = React.useState(defaultApp);
+
+  const envMap = React.useMemo(
+    () => appList.find(appObj => appObj.name === app)?.environments ?? {},
+    [app, appList]
+  );
+
+  const envList = React.useMemo(() => Object.keys(envMap), [envMap]);
+
+  const [env, setEnv] = React.useState(envList?.[0] ?? null);
+  const [ref, setRef] = React.useState(envMap[env]?.defaultRef ?? 'master');
+
+  // update environment when environment list changes
+  React.useEffect(() => void setEnv(envList?.[0] ?? null), [envList]);
+
+  // Update refs when app or env changes
+  React.useEffect(
+    () => void setRef(envMap?.[env]?.defaultRef ?? 'master'),
+    [env, envMap]
+  );
+
+  const [submitError, setSubmitError] = React.useState(false);
+  const [submitInProgress, setSubmitInProgress] = React.useState(false);
+
+  const startDeploy = React.useCallback(async () => {
+    const deployResp = await api.request('/deploys/', {
+      method: 'POST',
+      data: {app, env, ref},
     });
-  };
 
-  onChangeEnvironment = e => {
-    const val = e.target.value;
-    const config = val ? this.state.envMap[val] || {} : {};
-    this.setState({
-      env: val,
-      ref: config.defaultRef || 'master',
-    });
-  };
+    const result = await deployResp.json();
 
-  onChangeRef = e => {
-    this.setState({
-      ref: e.target.value,
-    });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    if (this.state.submitInProgress) {
-      return;
+    if (deployResp.ok) {
+      gotoDeploy(result);
+    } else {
+      setSubmitError(result.error);
+      setSubmitInProgress(false);
     }
+  }, [app, env, ref]);
 
-    const startDeploy = async () => {
-      const deployResp = await api.request('/deploys/', {
-        method: 'POST',
-        data: {
-          app: this.state.app,
-          env: this.state.env,
-          ref: this.state.ref,
-        },
-      });
+  /**
+   * Handles creating a deploy
+   */
+  const handleSubmit = React.useCallback(
+    e => {
+      e.preventDefault();
 
-      const result = await deployResp.json();
-
-      if (deployResp.ok) {
-        this.gotoDeploy(result);
-      } else {
-        this.setState({
-          submitError: result.error,
-          submitInProgress: false,
-        });
+      if (submitInProgress) {
+        return;
       }
-    };
 
-    // Ensure we don't double trigger a deploy by using the setState callback
-    this.setState({submitInProgress: true}, startDeploy);
-  };
+      setSubmitInProgress(true);
+      startDeploy();
+    },
+    [startDeploy, submitInProgress]
+  );
 
-  gotoDeploy = deploy => {
-    const {app, environment, number} = deploy;
-    browserHistory.push(`/deploys/${app.name}/${environment}/${number}`);
-  };
-
-  render() {
-    return (
-      <div className="create-deploy">
-        <div className="section">
-          <div className="section-header">
-            <h2>Create Deploy</h2>
-          </div>
-          {this.state.submitError && (
-            <div className="alert alert-block alert-danger">
-              <strong>Unable to create deploy:</strong>
-              <span>{this.state.submitError}</span>
-            </div>
-          )}
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <label>Application</label>
-              <select
-                className="form-control"
-                value={this.state.app}
-                onChange={this.onChangeApplication}
-              >
-                {this.props.appList.map(app => {
-                  return <option key={app.name}>{app.name}</option>;
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Environment</label>
-              <select
-                className="form-control"
-                value={this.state.env}
-                onChange={this.onChangeEnvironment}
-              >
-                {Object.keys(this.state.envMap).map(env => {
-                  return <option key={env}>{env}</option>;
-                })}
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Ref</label>
-              <input
-                type="text"
-                className="form-control"
-                onChange={this.onChangeRef}
-                placeholder="e.g. master"
-                value={this.state.ref}
-              />
-            </div>
-            <div className="submit-group">
-              <button
-                type="submit"
-                className="btn btn-primary"
-                disabled={this.state.submitInProgress}
-              >
-                Ship It!
-              </button>
-            </div>
-          </form>
+  return (
+    <div className="create-deploy">
+      <div className="section">
+        <div className="section-header">
+          <h2>Create Deploy</h2>
         </div>
+        {submitError && (
+          <div className="alert alert-block alert-danger">
+            <strong>Unable to create deploy: </strong>
+            <span>{submitError}</span>
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Application</label>
+            <select
+              className="form-control"
+              value={app}
+              onChange={e => setApp(e.target.value)}
+            >
+              {appList.map(a => (
+                <option key={a.name}>{a.name}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Environment</label>
+            <select
+              className="form-control"
+              value={env}
+              onChange={e => setEnv(e.target.value)}
+            >
+              {Object.keys(envMap).map(e => (
+                <option key={e}>{e}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Ref</label>
+            <input
+              type="text"
+              className="form-control"
+              onChange={e => setRef(e.target.value)}
+              placeholder="e.g. master"
+              value={ref}
+            />
+          </div>
+          <div className="submit-group">
+            <button type="submit" className="btn btn-primary" disabled={submitInProgress}>
+              Ship It!
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default CreateDeploy;
